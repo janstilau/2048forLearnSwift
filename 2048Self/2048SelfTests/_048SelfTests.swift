b76747c5e79e38c0156a096686743e89e8cfb962
janstilau <janstilau@gmail.com>
2016-09-20 13:52:45 +0800

    init

---
+++ 2048Self/2048SelfTests/_048SelfTests.swift
@@ -0,0 +1,36 @@
+//
+//  _048SelfTests.swift
+//  2048SelfTests
+//
+//  Created by jansti on 16/9/20.
+//  Copyright © 2016年 jansti. All rights reserved.
+//
+
+import XCTest
+@testable import _048Self
+
+class _048SelfTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
